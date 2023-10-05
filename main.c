21470b136389f4d084b7bc6d2093d7997ee582d8
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-10-05 16:30:42 -0500

    For añadido, auxiliar determina el numero de numeros divisibles del entero ingresado

--- main.c
+++ main.c
@@ -5,5 +5,18 @@ int main(void){
     int i, entero, aux;
     aux=0;
 
+    printf("Ingrese un numero entero\n");
+    scanf("%d", &entero);
+
+    for (i = 1; i < entero; i++)
+    {
+        if (entero%i==0)
+        {
+            aux+=aux;
+        }
+        
+    }
+    
+
     return 0;
 }
